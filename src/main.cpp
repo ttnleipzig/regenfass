38029e85fb82593c760468203cd2d5987fb9508a
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2023-03-16 21:05:44 +0100

    copied simple example

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <HCSR04.h>
 
-void setup() {
-  // put your setup code here, to run once:
+UltraSonicDistanceSensor distanceSensor(13, 12);  // Initialize sensor that uses digital pins 13 and 12.
+
+void setup () {
+    Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop () {
+    // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
+    Serial.println(distanceSensor.measureDistanceCm());
+    delay(500);
 }
