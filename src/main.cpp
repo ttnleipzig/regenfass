59afa9ef859e4b7f05b14487069f36a9bd409371
Pablo Clemente <paclema@gmail.com>
2023-03-28 18:10:32 +0200

    Add distance as global variable and include defines for UltraSonicDistanceSensor constructor

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 
 #include <HCSR04.h>
 
-UltraSonicDistanceSensor distanceSensor(5, 18);  // Initialize sensor that uses digital pins 13 and 12.
+#define TRIGGER_PIN 5
+#define ECHO_PIN 18
+#define MAX_DISTANCE 400
+
+UltraSonicDistanceSensor distanceSensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+float distance = -1;
 
 void setup () {
     Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
@@ -10,6 +15,7 @@ void setup () {
 
 void loop () {
     // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
-    Serial.println(distanceSensor.measureDistanceCm());
+    distance = distanceSensor.measureDistanceCm();
+    Serial.printf("Distance: %f cm", distance);
     delay(500);
 }
