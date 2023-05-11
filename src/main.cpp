d04dcf6fffd191f2240d4014cf961ecbfdacf6dc
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2023-05-11 19:54:02 +0200

    include LORA, WIP

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #ifdef SENSOR_TYPE_HCSR04
 #include <HCSR04.h>
 
+<<<<<<< HEAD
 #define DISTANCE_MAX 400
 
 UltraSonicDistanceSensor distanceSensor(SENSOR_TYPE_HCSR04_PIN_TRIGGER, SENSOR_TYPE_HCSR04_PIN_ECHO, DISTANCE_MAX);
@@ -46,6 +47,47 @@ void setup()
 #endif
 
     Serial.println("Started");
+=======
+#include <LoRa_E220.h>
+
+// https://registry.platformio.org/libraries/xreef/EByte%20LoRa%20E220%20library/examples/01_getConfiguration/01_getConfiguration.ino
+LoRa_E220 e220ttl(&Serial1, 2, 4, 6); //  RX AUX M0 M1
+
+UltraSonicDistanceSensor distanceSensor(5, 18);  // Initialize sensor that uses digital pins 13 and 12.
+
+void setup () {
+    Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
+
+    while(!Serial){};
+	delay(500);
+
+	Serial.println();
+
+	// Startup all pins and UART
+	e220ttl.begin();
+
+	ResponseStructContainer c;
+	c = e220ttl.getConfiguration();
+	// It's important get configuration pointer before all other operation
+	Configuration configuration = *(Configuration*) c.data;
+	Serial.println(c.status.getResponseDescription());
+	Serial.println(c.status.code);
+
+	printParameters(configuration);
+
+	ResponseStructContainer cMi;
+	cMi = e220ttl.getModuleInformation();
+	// It's important get information pointer before all other operation
+	ModuleInformation mi = *(ModuleInformation*)cMi.data;
+
+	Serial.println(cMi.status.getResponseDescription());
+	Serial.println(cMi.status.code);
+
+	printModuleInformation(mi);
+
+	c.close();
+	cMi.close();
+>>>>>>> 8deb890 (include LORA, WIP)
 }
 
 void loop()
@@ -62,3 +104,42 @@ void loop()
     Serial.printf("Distance: %f cm\n", distance);
     delay(500);
 }
+<<<<<<< HEAD
+=======
+
+void printParameters(struct Configuration configuration) {
+	Serial.println("----------------------------------------");
+
+	Serial.print(F("HEAD : "));  Serial.print(configuration.COMMAND, HEX);Serial.print(" ");Serial.print(configuration.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(configuration.LENGHT, HEX);
+	Serial.println(F(" "));
+	Serial.print(F("AddH : "));  Serial.println(configuration.ADDH, HEX);
+	Serial.print(F("AddL : "));  Serial.println(configuration.ADDL, HEX);
+	Serial.println(F(" "));
+	Serial.print(F("Chan : "));  Serial.print(configuration.CHAN, DEC); Serial.print(" -> "); Serial.println(configuration.getChannelDescription());
+	Serial.println(F(" "));
+	Serial.print(F("SpeedParityBit     : "));  Serial.print(configuration.SPED.uartParity, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTParityDescription());
+	Serial.print(F("SpeedUARTDatte     : "));  Serial.print(configuration.SPED.uartBaudRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTBaudRateDescription());
+	Serial.print(F("SpeedAirDataRate   : "));  Serial.print(configuration.SPED.airDataRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getAirDataRateDescription());
+	Serial.println(F(" "));
+	Serial.print(F("OptionSubPacketSett: "));  Serial.print(configuration.OPTION.subPacketSetting, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getSubPacketSetting());
+	Serial.print(F("OptionTranPower    : "));  Serial.print(configuration.OPTION.transmissionPower, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getTransmissionPowerDescription());
+	Serial.print(F("OptionRSSIAmbientNo: "));  Serial.print(configuration.OPTION.RSSIAmbientNoise, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getRSSIAmbientNoiseEnable());
+	Serial.println(F(" "));
+	Serial.print(F("TransModeWORPeriod : "));  Serial.print(configuration.TRANSMISSION_MODE.WORPeriod, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getWORPeriodByParamsDescription());
+	Serial.print(F("TransModeEnableLBT : "));  Serial.print(configuration.TRANSMISSION_MODE.enableLBT, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getLBTEnableByteDescription());
+	Serial.print(F("TransModeEnableRSSI: "));  Serial.print(configuration.TRANSMISSION_MODE.enableRSSI, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getRSSIEnableByteDescription());
+	Serial.print(F("TransModeFixedTrans: "));  Serial.print(configuration.TRANSMISSION_MODE.fixedTransmission, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getFixedTransmissionDescription());
+
+
+	Serial.println("----------------------------------------");
+}
+void printModuleInformation(struct ModuleInformation moduleInformation) {
+	Serial.println("----------------------------------------");
+	Serial.print(F("HEAD: "));  Serial.print(moduleInformation.COMMAND, HEX);Serial.print(" ");Serial.print(moduleInformation.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(moduleInformation.LENGHT, DEC);
+
+	Serial.print(F("Model no.: "));  Serial.println(moduleInformation.model, HEX);
+	Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, HEX);
+	Serial.print(F("Features : "));  Serial.println(moduleInformation.features, HEX);
+	Serial.println("----------------------------------------");
+}
+>>>>>>> 8deb890 (include LORA, WIP)
