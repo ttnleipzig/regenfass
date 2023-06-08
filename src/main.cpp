4291fbff1dfe7ba6e4b9a6c30df7c0ac0c0ebe6b
DevMiner <devminer@devminer.xyz>
2023-06-08 20:48:51 +0200

    impl: DS18B20

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,16 @@ UltraSonicDistanceSensor distanceSensor(SENSOR_TYPE_HCSR04_PIN_TRIGGER, SENSOR_T
 #include <Wire.h>
 #include <VL53L1X.h>
 VL53L1X sensor;
+
+#elif SENSOR_TYPE_DS18B20
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+OneWire oneWire(SENSOR_TYPE_DS18B20_DATA_PIN);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
 #else
 #warning "Sensor type not selected. Add SENSOR_TYPE_HCSR04 or SENSOR_TYPE_VL53L1X in your environment build_plags in platformio.ini"
 #endif
@@ -45,6 +55,10 @@ void setup()
     sensor.startContinuous(50);
 #endif
 
+#ifdef SENSOR_TYPE_DS18B20
+    sensors.begin();
+#endif
+
     Serial.println("Started");
 }
 
@@ -54,11 +68,22 @@ void loop()
 // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
 #ifdef SENSOR_TYPE_HCSR04
     distance = distanceSensor.measureDistanceCm();
+
+    Serial.printf("Distance: %f cm\n", distance);
 #elif SENSOR_TYPE_VL53L1X
     sensor.read();
     distance = sensor.ranging_data.range_mm / 10.0;
-#endif
 
     Serial.printf("Distance: %f cm\n", distance);
+#elif SENSOR_TYPE_DS18B20
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  float temperatureF = sensors.getTempFByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+  Serial.print(temperatureF);
+  Serial.println("ºF");
+#endif
+
     delay(500);
 }
