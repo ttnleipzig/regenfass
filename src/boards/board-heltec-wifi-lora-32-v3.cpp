e0c034ca4c810f279b50a665acbf3b6b0adcfdd4
André Lademann <alademann@thinkport.digital>
2023-08-14 21:07:29 +0200

    Change environment

--- src/boards/board-heltec-wifi-lora-32-v3.cpp
+++ src/boards/board-heltec-wifi-lora-32-v3.cpp
@@ -1,3 +1,4 @@
+#include "Arduino.h"
 #include <Wire.h>
 #include "HT_SSD1306Wire.h"
 #include "../assets/bitmaps.h"
@@ -12,6 +13,21 @@ namespace Board
 {
     namespace HeltecWifiLora32V3
     {
+
+        /*license for Heltec ESP32 LoRaWan, quary your ChipID relevant license: http://resource.heltec.cn/search */
+        uint32_t license[4] = {0x00000000, 0x00000000, 0x00000000, 0x00000000};
+        /* OTAA para*/
+        uint8_t DevEui[] = {0x22, 0x32, 0x33, 0x00, 0x00, 0x88, 0x88, 0x02};
+        uint8_t AppEui[] = {0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x02, 0xB1, 0x8A};
+        uint8_t AppKey[] = {0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x66, 0x01};
+
+        /* ABP para*/
+        uint8_t NwkSKey[] = {0x15, 0xb1, 0xd0, 0xef, 0xa4, 0x63, 0xdf, 0xbe, 0x3d, 0x11, 0x18, 0x1e, 0x1e, 0xc7, 0xda, 0x85};
+        uint8_t AppSKey[] = {0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef, 0x67};
+        uint32_t DevAddr = (uint32_t)0x007e6ae1;
+
+        /*LoraWan channelsmask, default channels 0-7*/
+        uint16_t userChannelsMask[6] = {0x00FF, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000};
 
         void VextON(void)
         {
@@ -47,16 +63,88 @@ namespace Board
         void loopDisplay()
         {
         }
+        void prepareTxFrame(uint8_t port)
+        {
+            appDataSize = 4; // AppDataSize max value is 64
+            // Format the data to bytes
+            appData[0] = 0x00;
+            appData[1] = 0x01;
+            appData[2] = 0x02;
+            appData[3] = 0x03;
+        }
+
+        void setupLora()
+        {
+            if (mcuStarted == 0)
+            {
+                LoRaWAN.displayMcuInit();
+            }
+            Serial.begin(115200);
+            while (!Serial)
+                ;
+            SPI.begin(SCK, MISO, MOSI, SS);
+            Mcu.init(SS, RST_LoRa, DIO0, DIO1, license);
+            deviceState = DEVICE_STATE_INIT;
+        }
+
+        void loopLora()
+        {
+            switch (deviceState)
+            {
+            case DEVICE_STATE_INIT:
+            {
+#if (LORAWAN_DEVEUI_AUTO)
+                LoRaWAN.generateDeveuiByChipID();
+#endif
+                LoRaWAN.init(loraWanClass, loraWanRegion);
+                break;
+            }
+            case DEVICE_STATE_JOIN:
+            {
+                LoRaWAN.displayJoining();
+                LoRaWAN.join();
+                break;
+            }
+            case DEVICE_STATE_SEND:
+            {
+                LoRaWAN.displaySending();
+                prepareTxFrame(appPort);
+                LoRaWAN.send(loraWanClass);
+                deviceState = DEVICE_STATE_CYCLE;
+                break;
+            }
+            case DEVICE_STATE_CYCLE:
+            {
+                // Schedule next packet transmission
+                txDutyCycleTime = appTxDutyCycle + randr(-APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND);
+                LoRaWAN.cycle(txDutyCycleTime);
+                deviceState = DEVICE_STATE_SLEEP;
+                break;
+            }
+            case DEVICE_STATE_SLEEP:
+            {
+                LoRaWAN.displayAck();
+                LoRaWAN.sleep(loraWanClass, debugLevel);
+                break;
+            }
+            default:
+            {
+                deviceState = DEVICE_STATE_INIT;
+                break;
+            }
+            }
+        }
 
         void setup()
         {
             Serial.println("Setup: Board Heltec Wifi Lora 32 V3");
             // Heltec.begin(true, false, true);
-            setupDisplay();
+            // setupDisplay();
+            // setupLora();
         }
         void loop()
         {
-            loopDisplay();
+            // loopDisplay();
         }
 
     } // namespace HeltecV3
--- src/boards/board-heltec-wifi-lora-32-v3.h
+++ src/boards/board-heltec-wifi-lora-32-v3.h
@@ -4,7 +4,16 @@ namespace Board
 {
     namespace HeltecWifiLora32V3
     {
+
+        void VextON(void);
+        void VextOFF(void);
+        void prepareTxFrame(uint8_t port);
+
         void setup();
+        void setupDisplay();
+        void setuoLora();
         void loop();
+        void loopDisplay();
+        void loopLora();
     }
 }
