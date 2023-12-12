7e89bdd1b6970c4d64ee2384ff5e8e0ee5d6c20f
André Lademann <alademann@thinkport.digital>
2023-12-13 00:22:11 +0100

    ♻️ refactor: Organize build_flags and lib_deps in platformio.ini
    This commit refactors the build_flags and lib_deps sections in the platformio.ini file. It removes unnecessary comments and whitespace, and reorders the flags and dependencies for better readability. The changes ensure consistency and improve code maintainability.

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,11 @@
 #include "displays/display-sd1306.h"
 #endif
 
+// LoRaWAN
+#ifdef LORAWAN_ENABLED
+#include "lora/lora-wan.h"
+#endif
+
 // Main functions
 void setup()
 {
@@ -33,14 +38,15 @@ void setup()
     }
 #endif
     Serial.println("\033[32m\n\n*********************************************************");
-    Serial.println("🌈\tStarting regenfass");
+    Serial.println("🌈\t\t\tStarting regenfass");
     Serial.println("*********************************************************\033[0m\n");
 
-    // Debugging
+    // Debugging information
     if (CORE_DEBUG_LEVEL > 1)
-        log_w("Debug:\tenabled");
+        Serial.println("Debug:\tenabled");
     else
-        log_i("Debug:\tdisabled");
+        Serial.println("Debug:\tdisabled");
+    Serial.println("\n\n\n");
 
 // Sensors
 #if SENSOR_HCSR04
@@ -57,6 +63,11 @@ void setup()
 // Display SD1306
 #ifdef DISPLAY_SD1306
     Display::SD1306::setup();
+#endif
+
+// LoRaWAN
+#ifdef LORAWAN_ENABLED
+    Lora::Wan::setup();
 #endif
 }
 
@@ -78,106 +89,9 @@ void loop()
 #ifdef DISPLAY_SD1306
     Display::SD1306::loop();
 #endif
+
+// LoRaWAN
+#ifdef LORAWAN_ENABLED
+    Lora::Wan::loop();
+#endif
 }
-
-/*
-TAKEN FROM LOOP:
-
-distance = distanceSensor.measureDistanceCm();
-
-    Serial.printf("Distance: %f cm\n", distance);
-#elif SENSOR_VL53L1X
-    sensor.read();
-    distance = sensor.ranging_data.range_mm / 10.0;
-
-  #ifdef LORE_ENABLED
-    if(distance <= 10){
-      publish2TTN();
-    }
-    loraLoop();
-  #endif
-
-  // Print to serial every 500 miliseconds
-  unsigned long current_time = millis();
-  if (current_time - last_print_time >= 500) {
-    last_print_time = current_time;
-    Serial.printf("Distance: %f cm\n", distance);
-#elif SENSOR_DS18B20
-  sensors.requestTemperatures();
-  float temperatureC = sensors.getTempCByIndex(0);
-  float temperatureF = sensors.getTempFByIndex(0);
-  Serial.print(temperatureC);
-  Serial.println("ºC");
-  Serial.print(temperatureF);
-  Serial.println("ºF");
-#endif
-
-// Read the distance
-#if SENSOR_HCSR04
-    distance = Sensor::HCSR04::measureDistanceCm();
-#elif SENSOR_VL53L1X
-    distance = Sensor::VL53L1X::measureDistanceCm();
-#endif
-
-    // Print the distance
-  // Print to serial every 500 miliseconds
-  unsigned long current_time = millis();
-  if (current_time - last_print_time >= 500) {
-    last_print_time = current_time;
-    Serial.printf("Distance: %f cm\n", distance);
-#elif SENSOR_DS18B20
-  sensors.requestTemperatures();
-  float temperatureC = sensors.getTempCByIndex(0);
-  float temperatureF = sensors.getTempFByIndex(0);
-  Serial.print(temperatureC);
-  Serial.println("ºC");
-  Serial.print(temperatureF);
-  Serial.println("ºF");
-#endif
-*/
-
-/*
-// Global variables
-float distance = -1;
-
-unsigned long last_print_time = 0;
-
-
-void setup () {
-
-  #ifdef WAIT_SERIAL
-  while (!Serial) {}
-  #endif
-
-  Serial.begin(115200);  // We initialize serial connection so that we could print values from sensor.
-  Serial.println("Starting...");
-
-    Serial.begin(115200); // We initialize serial connection so that we could print values from sensor.
-    Serial.println("Starting...");
-
-#ifdef SENSOR_VL53L1X
-    Wire.begin();
-    Wire.setClock(400000); // use 400 kHz I2C
-
-    sensor.setTimeout(500);
-    if (!sensor.init())
-    {
-        Serial.println("Failed to detect and initialize VL53L1X sensor!");
-        while (1)
-            ;
-    }
-    sensor.setDistanceMode(VL53L1X::Long);
-    sensor.setMeasurementTimingBudget(50000);
-    sensor.startContinuous(50);
-  #endif
-
-  #ifdef LORE_ENABLED
-    loraSetup();
-  #endif
-
-  Serial.println("Started");
-
-
-#ifdef SENSOR_DS18B20
-    sensors.begin();
-*/
