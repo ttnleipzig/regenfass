2ab130c6344c8c365865588841b31d6e3f63ac18
André Lademann <alademann@thinkport.digital>
2023-12-12 21:36:07 +0100

    ✨ feat: Update platformio.ini and button code
    - Updated the `platformio.ini` file to include additional build flags for debugging and configuration.
    - Added a log statement in the `setup()` function of the `Button` namespace instead of using Serial.println().
    - Modified the button logic to use log statements for pressed and released states.
    - Added `esp32-hal-log.h` library to the `main.cpp` file.
    
    The changes ensure better debugging and logging capabilities for the project.

--- src/button/button.cpp
+++ src/button/button.cpp
@@ -5,16 +5,15 @@ namespace Button
 {
     void setup()
     {
-        if (DEBUG_LEVEL > 0)
-            Serial.println("Setup:\tButton");
+        log_i("Setup:\tButton");
         pinMode(BUTTON_PIN, INPUT_PULLUP);
     }
     void loop()
     {
         // Check if the button is pressed
-        if (digitalRead(0) == LOW && DEBUG_LEVEL > 1)
-            Serial.println("Button:\tpressed");
-        else if (DEBUG_LEVEL > 2)
-            Serial.println("Button:\treleased");
+        if (digitalRead(0) == LOW)
+            log_d("Button:\tpressed");
+        else
+            log_v("Button:\treleased");
     }
 } // namespace Button
