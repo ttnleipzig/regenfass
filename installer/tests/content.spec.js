e0f5d328cca15c987c832e8cd5e1eeb8a7eb4f67
André Lademann <alademann@thinkport.digital>
2024-07-09 21:32:55 +0200

    style(index.html): wrap main content in sections for better semantic structure
    style: reformat HTML for better readability and consistency
    
    feat(UI): add new button for configuration submission and log display area for better user interaction and feedback
    
    style(index.html): fix button id indentation for better readability
    
    test(content.spec.js): add detailed tests for installation and configuration sections
    
    Refactor tests to include sections for better organization and readability.

--- installer/tests/content.spec.js
+++ installer/tests/content.spec.js
@@ -9,7 +9,38 @@ test.describe('Content', () => {
 	test('Has a main tag inside body', async ({page}) => {
 		await expect(page.locator('body main')).toBeVisible()
 	})
-	test('Has a welcome message', async ({page}) => {
-		await expect(page.getByRole('heading', {name: 'Hi there!'})).toBeVisible()
+
+	// section Welcome
+	test.describe('Section: Welcome', () => {
+		test('Has a welcome message', async ({page}) => {
+			await expect(page.getByRole('heading', {name: 'Hi there!'})).toBeVisible()
+		})
+	})
+
+	// Section Installation & Configuration
+	test.describe('Section: Installation & configuration', () => {
+		test('Has a heading', async ({page}) => {
+			await expect(page.getByRole('heading', {name: 'Installation & configuration'})).toBeVisible()
+		})
+		test('Has a h2 tag', async ({page}) => {
+			await expect(page.locator('main section:nth-child(2) h2')).toBeVisible()
+		})
+		test.describe('Step 1: Installation', () => {
+			test('Has a details summary', async ({page}) => {
+				await expect(page.locator('#step-installation summary')).toBeVisible()
+				await expect(page.locator('#step-installation summary')).toContainText('1. Installation')
+			})
+
+			// On clicking the summary, the details element should be visible
+			test('Has a details element which reacts on click', async ({page}) => {
+				await page.locator('#step-installation summary').click()
+				await expect(page.locator('#step-installation details')).toBeHidden()
+			})
+
+			// Section contains "Connect your microcontroller with an USB cable to your computer."
+			test('Has a details element with a ordered list', async ({page}) => {
+				await expect(page.locator('#step-installation details ol')).toBeDefined()
+			})
+		})
 	})
 })
