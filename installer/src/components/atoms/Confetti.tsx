a88ef756e8cf7e7024dfc20f813d6c74f5185852
André Lademann <alademann@thinkport.digital>
2025-06-10 23:00:31 +0200

    chore(gitignore): add storybook log and static files to ignore list
    feat(installer): update package dependencies to latest versions
    
    feat(installer): add Confetti component with CSS for visual effects
    
    style(installer): add ConfettiSpinner CSS for drawing canvas styling
    
    feat(ConfettiSpinner): add new ConfettiSpinner component
    
    Introduce a new ConfettiSpinner component to enhance the UI
    with a visually appealing confetti animation. This component
    utilizes canvas to render animated particles, providing a
    dynamic and engaging user experience. The addition aims to
    improve user interaction and visual feedback within the
    application.

---
+++ installer/src/components/atoms/Confetti.tsx
@@ -0,0 +1,18 @@
+import "./Confetti.css";
+
+export default function Confetti() {
+	return (
+		<div class="confetti-container">
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+			<div class="confetti"></div>
+		</div>
+	);
+}
