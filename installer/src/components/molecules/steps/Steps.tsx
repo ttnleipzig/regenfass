e58ca20ac4509eef9d57528ed85f4c37579d4188
DevMiner <devminer@devminer.xyz>
2025-12-09 23:42:12 +0100

    fix import error

--- installer/src/components/molecules/steps/Steps.tsx
+++ installer/src/components/molecules/steps/Steps.tsx
@@ -15,6 +15,7 @@ import {
 import {
 	TextFieldInput,
 	TextFieldLabel,
+	TextFieldRoot,
 } from "@/components/forms/TextField.tsx";
 import { setupStateMachine } from "@/libs/install/state.ts";
 import { createBrowserInspector } from "@statelyai/inspect";
